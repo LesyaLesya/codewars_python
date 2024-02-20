b00208a319a5438d4cfa3ca725c5c9f5ce3693c7
lesya <lesya@MacBook-Pro-lesya.local>
2024-02-20 15:15:07 +0300

    added new katas solutions

--- 7kyu.py
+++ 7kyu.py
@@ -2625,3 +2625,30 @@ def count_correct_characters(correct, guess):
 
 print(count_correct_characters("dog", "car"))
 print(count_correct_characters("dog", "dog"))
+
+
+"""
+Remove the minimum
+
+Given an array of integers, remove the smallest value. Do not mutate the original array/list.
+If there are multiple elements with the same value, remove the one with the lowest index. 
+If you get an empty array/list, return an empty array/list.
+
+Don't change the order of the elements that are left.
+Examples
+
+* Input: [1,2,3,4,5], output = [2,3,4,5]
+* Input: [5,3,2,1,4], output = [5,3,2,4]
+* Input: [2,2,1,2,1], output = [2,2,2,1]
+"""
+
+print('*** Remove the minimum ***')
+
+
+def remove_smallest(numbers):
+    if len(numbers) < 2: return []
+    idx = numbers.index(min(numbers))
+    return [value for i, value in enumerate(numbers) if i != idx]
+
+
+print(remove_smallest([1,2,3,4,5]))
